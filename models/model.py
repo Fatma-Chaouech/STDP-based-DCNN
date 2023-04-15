792eaef28674863a03a3e54e9bfa6346892b765f
Fatma-Chaouech <fatmachaouech@insat.u-carthage.tn>
2023-04-15 17:47:22 +0100

    added tmp directory

--- models/model.py
+++ models/model.py
@@ -10,13 +10,13 @@ class Network(nn.Module):
         self.device = device
 
         self.conv1 = snn.Convolution(
-            in_channels=2, out_channels=30, kernel_size=5)
+            in_channels=2, out_channels=32, kernel_size=5)
         self.conv1_threshold = 10
         self.conv1_kwinners = 5
         self.conv1_inhibition_rad = 2
 
         self.conv2 = snn.Convolution(
-            in_channels=30, out_channels=100, kernel_size=5)
+            in_channels=32, out_channels=150, kernel_size=2)
         self.conv2_threshold = 1
         self.conv2_kwinners = 8
         self.conv2_inhibition_rad = 1
@@ -85,8 +85,9 @@ class Network(nn.Module):
 
             pot = self.conv2(padded)
             spk, pot = sf.fire(pot, self.conv2_threshold, True)
-            pooled_spk, _ = torch.max(spk.reshape(spk.size(1), -1), dim=1)
-            spk_out = pooled_spk.view(1, spk.size(1))
+            # pooled_spk, _ = torch.max(spk.reshape(spk.size(1), -1), dim=1)
+            # spk_out = pooled_spk.view(1, spk.size(1))
+            spk_out = sf.pooling(spk, 2, 2, 1)
             return spk_out
 
     def stdp(self, layer_idx):
